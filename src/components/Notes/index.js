aeba5606da9aa9522458ee602d5dad60ecae4f63
Rodolfo Saldivar <rodolfo.saldivar@udem.edu>
2023-08-22 15:59:56 -0600

    scroll to top

--- src/components/Notes/index.js
+++ src/components/Notes/index.js
@@ -1,6 +1,8 @@
 import React, { useEffect } from "react";
 import { CONTENT } from "utils/constants";
 import SaveNote from "components/Notes/SaveNote";
+import NorthIcon from "@mui/icons-material/North";
+import IconButton from "@mui/material/IconButton";
 import NotesHeader from "components/Notes/NotesHeader";
 import DisplayNotes from "components/Notes/DisplayNotes";
 import { getAllNotesFromFirebase } from "utils/firebase";
@@ -18,11 +20,26 @@ const Notes = () => {
     };
   }, []);
 
+  const scrollTop = () => window.scrollTo({ top: 0, behavior: "smooth" });
+
   return (
     <ContentContainer>
       <NotesHeader />
       <DisplayNotes />
       <SaveNote />
+
+      <div className="sticky bottom-5 float-right mt-10">
+        <div className="rounded-full bg-blue-200 opacity-80">
+          <IconButton
+            size="large"
+            color="primary"
+            aria-label="update"
+            onClick={scrollTop}
+          >
+            <NorthIcon fontSize="inherit" />
+          </IconButton>
+        </div>
+      </div>
     </ContentContainer>
   );
 };
