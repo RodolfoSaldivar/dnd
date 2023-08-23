ff3aa026f9e444785cd3c64be5731a65bfa1df67
Rodolfo Saldivar <rodolfo.saldivar@udem.edu>
2023-08-23 14:18:03 -0600

    dont show hidden notes for others

--- src/components/Notes/DisplayNotes.jsx
+++ src/components/Notes/DisplayNotes.jsx
@@ -1,13 +1,19 @@
 import React from "react";
 import { useNotesStore } from "stores/notesStore";
+import { useLoggedUserStore } from "stores/loggedUser";
 import NoteAccordion from "components/Notes/NoteAccordion";
 
 const DisplayNotes = () => {
+  const loggedUserId = useLoggedUserStore(state => state.userId);
   const notesToDisplay = useNotesStore(state => state.notesToDisplay);
+
+  const visibleNotes = [...notesToDisplay.values()].filter(
+    currNote => !currNote.hidden || currNote.ownerId === loggedUserId,
+  );
 
   return (
     <div className="mt-7">
-      {[...notesToDisplay.values()].map(currNote => (
+      {visibleNotes.map(currNote => (
         <NoteAccordion key={currNote.id} note={currNote} />
       ))}
     </div>
